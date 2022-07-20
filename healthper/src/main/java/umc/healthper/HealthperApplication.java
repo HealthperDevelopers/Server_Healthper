abb746ccbb69dcfa011652ba120a77f0225268c2
Wo-ogie <siwall0105@gmail.com>
2022-07-20 18:43:08 +0900

    Feat: create and set backend project

---
+++ healthper/src/main/java/umc/healthper/HealthperApplication.java
@@ -0,0 +1,13 @@
+package umc.healthper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthperApplication.class, args);
+	}
+
+}
