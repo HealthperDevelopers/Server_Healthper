004338c558ddc54a7cd80d026471c4fcd231cfa1
Wo-ogie <siwall0105@gmail.com>
2022-07-26 15:25:52 +0900

    feat: MappedSuperClass(BaseTimeEntity) 구현 및 Spring에 적용

--- healthper/src/main/java/umc/healthper/HealthperApplication.java
+++ healthper/src/main/java/umc/healthper/HealthperApplication.java
@@ -2,8 +2,10 @@ package umc.healthper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HealthperApplication {
 
 	public static void main(String[] args) {
