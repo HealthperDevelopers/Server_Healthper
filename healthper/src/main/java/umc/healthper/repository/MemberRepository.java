d9c18fdef4a158801f6ec906c58228906e3ccd23
Wo-ogie <siwall0105@gmail.com>
2022-07-26 15:59:16 +0900

    feat: 회원(Member) Service, Repository 일부 구현 (Rough, Post 기능 테스트를 위해 간단하게 구현)

---
+++ healthper/src/main/java/umc/healthper/repository/MemberRepository.java
@@ -0,0 +1,46 @@
+package umc.healthper.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import umc.healthper.domain.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    /**
+     * 회원 등록
+     */
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    /**
+     * 회원 조회
+     */
+    public Member findById(int id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m")
+                .getResultList();
+    }
+
+    public List<Member> findByNickName(String nickName) {
+        return em.createQuery("select m from Member m where m.nickName = :name")
+                .setParameter("name", nickName)
+                .getResultList();
+    }
+
+    public List<Member> findByKakaoIdx(Long kakaoIdx) {
+        return em.createQuery("select m from Member m where m.kakaoIdx = :kakaoIdx")
+                .setParameter("kakaoIdx", kakaoIdx)
+                .getResultList();
+    }
+}
