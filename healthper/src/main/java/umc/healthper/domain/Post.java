15c3bfdabc3eba0c187549f376598ab2c1060e99
Wo-ogie <siwall0105@gmail.com>
2022-08-01 20:25:41 +0900

    refactor(Post): Setter private으로 설정

--- healthper/src/main/java/umc/healthper/domain/Post.java
+++ healthper/src/main/java/umc/healthper/domain/Post.java
@@ -10,9 +10,11 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+import static umc.healthper.domain.PostStatus.*;
+
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post extends BaseTimeEntity {
 
@@ -21,7 +23,7 @@ public class Post extends BaseTimeEntity {
     @Column(name = "post_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
@@ -37,7 +39,7 @@ public class Post extends BaseTimeEntity {
 
     //== Constructor ==//
     public Post(Member member, String title, String content) {
-        this(member, title, content, PostStatus.NORMAL);
+        this(member, title, content, NORMAL);
     }
 
     public Post(Member member, String title, String content, PostStatus postStatus) {
@@ -47,9 +49,38 @@ public class Post extends BaseTimeEntity {
         this.postStatus = postStatus;
     }
 
+    //== Setter ==//
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    private void setMember(Member member) {
+        this.member = member;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    private void setContent(String content) {
+        this.content = content;
+    }
+
+    private void setPostStatus(PostStatus postStatus) {
+        this.postStatus = postStatus;
+    }
+
+    private void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
     //== 수정 메서드 ==//
     public void change(String title, String content) {
         this.setTitle(title);
         this.setContent(content);
+    }
+
+    public void delete() {
+        this.setPostStatus(REMOVED);
     }
 }
