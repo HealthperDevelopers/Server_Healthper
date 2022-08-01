cb1877a9986692575f00bc62682812fffd5a311e
Wo-ogie <siwall0105@gmail.com>
2022-08-01 22:21:51 +0900

    feat(Comment): 댓글 추가 기능 구현 (Service, Repository, Domain)

--- healthper/src/main/java/umc/healthper/domain/Comment.java
+++ healthper/src/main/java/umc/healthper/domain/Comment.java
@@ -1,10 +1,15 @@
 package umc.healthper.domain;
 
 import lombok.Getter;
-import lombok.Setter;
 import umc.healthper.global.BaseTimeEntity;
 
 import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.*;
+import static umc.healthper.domain.CommentStatus.*;
 
 @Entity
 @Getter
@@ -15,24 +20,71 @@ public class Comment extends BaseTimeEntity {
     @Column(name = "comment_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    private String Content;
+
+    @Enumerated(EnumType.STRING)
+    private CommentStatus status;    // NORMAL, REMOVED, BLOCKED
+
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Comment parent;
 
-//    @OneToMany(mappedBy = "parent")
-//    private List<Comment> child = new ArrayList<>();
+    @OneToMany(mappedBy = "parent")
+    private List<Comment> children = new ArrayList<>();
+
+    //== 생성 메서드 ==//
+    public static Comment createComment(Member member, Post post, String content) {
+        Comment comment = new Comment();
+        comment.setMember(member);
+        comment.setPost(post);
+        comment.setContent(content);
+        comment.setStatus(NORMAL);
+        return comment;
+    }
+
+    public static Comment createNestedComment(Member member, Post post, Comment parent, String content) {
+        Comment comment = createComment(member, post, content);
+        parent.addChildComment(comment);
+        return comment;
+    }
 
     //== 연관관계 편의 Method ==//
-//    public void addChildComment(Comment child) {
-//        this.child.add(child);
-//        child.setParent(this);
-//    }
+    public void setPost(Post post) {
+        this.post = post;
+        post.getComments().add(this);
+    }
+
+    public void addChildComment(Comment child) {
+        this.getChildren().add(child);
+        child.setParent(this);
+    }
+
+    //== Setter ==//
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    private void setMember(Member member) {
+        this.member = member;
+    }
+
+    private void setContent(String content) {
+        Content = content;
+    }
+
+    private void setStatus(CommentStatus status) {
+        this.status = status;
+    }
+
+    private void setParent(Comment parent) {
+        this.parent = parent;
+    }
 }
---
+++ healthper/src/main/java/umc/healthper/service/CommentService.java
@@ -0,0 +1,24 @@
+package umc.healthper.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.healthper.domain.Comment;
+import umc.healthper.domain.Post;
+import umc.healthper.repository.CommentRepository;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    /**
+     * Comment 등록(저장)
+     */
+    @Transactional
+    public Comment saveComment(Comment comment) {
+        return commentRepository.save(comment);
+    }
+}
