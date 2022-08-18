a58f5d4d9165e257dde563691e831098c208cc18
Wo-ogie <siwall0105@gmail.com>
2022-08-18 12:10:42 +0900

    docs: `Post` Swagger API specifications 작성

--- healthper/src/main/java/umc/healthper/api/PostController.java
+++ healthper/src/main/java/umc/healthper/api/PostController.java
@@ -1,9 +1,16 @@
 package umc.healthper.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import umc.healthper.domain.Member;
 import umc.healthper.domain.Post;
@@ -15,6 +22,7 @@ import javax.validation.Valid;
 
 import static org.springframework.data.domain.Sort.Direction.*;
 
+@Tag(name = "Post", description = "게시글 관련 API")
 @RestController
 @RequiredArgsConstructor
 public class PostController {
@@ -22,9 +30,14 @@ public class PostController {
     private final PostService postService;
     private final MemberService memberService;
 
-    /**
-     * 게시글 등록
-     */
+
+    @Operation(summary = "게시글 생성",
+            description = "게시글 정보를 받아 새로운 게시글을 생성합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = CreatePostResponseDto.class))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST", content = @Content(schema = @Schema(implementation = ResponseEntity.class))),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = @Content(schema = @Schema(implementation = ResponseEntity.class)))
+    })
     @PostMapping("/post")
     public CreatePostResponseDto savePost(@RequestBody @Valid CreatePostRequestDto request) {
         Member member = memberService.findById(request.getMemberId());
@@ -34,36 +47,35 @@ public class PostController {
         return new CreatePostResponseDto(id);
     }
 
-    /**
-     * 게시글 조회
-     */
+    @Operation(summary = "게시글 조회",
+            description = "`posdId`에 해당하는 게시글을 조회합니다. 댓글과 대댓글 목록도 함께 반환됩니다.\n\n" +
+                    "`postStatus`: `NORMAL`, `REMOVED`(삭제된 게시글), `BLOCKED`(차단된 게시글)")
     @GetMapping("/post/{postId}")
     public PostResponseDto getPostInfo(@PathVariable Long postId) {
         Post findPost = postService.findPost(postId);
         return new PostResponseDto(findPost);
     }
 
-    /**
-     * 게시글 목록 조회
-     */
+    @Operation(summary = "게시글 목록 조회",
+            description = "`posdId`에 해당하는 게시글을 조회합니다.\n\n" +
+                    "댓글과 대댓글 목록도 함께 반환됩니다.")
     @GetMapping("/posts")
     public Page<ListPostResponseDto> getPosts(@PageableDefault(size = 30, sort = "createdAt", direction = DESC) Pageable pageable) {
         return postService.findPosts(pageable)
                 .map(ListPostResponseDto::new);
     }
 
-    /**
-     * 게시글 수정
-     */
+    @Operation(summary = "게시글 수정",
+            description = "게시글 수정에 관한 데이터들을 전달받아 `posdId`에 해당하는 게시글을 수정합니다.")
     @PatchMapping("/post/{postId}")
     public void updatePost(@PathVariable Long postId,
                            @RequestBody @Valid UpdatePostRequestDto request) {
         postService.updatePost(postId, request);
     }
 
-    /**
-     * 게시글 삭제
-     */
+    @Operation(summary = "게시글 삭제",
+            description = "`posdId`에 해당하는 게시글을 삭제합니다.\n\n" +
+                    "실제로 DB에서 삭제되지는 않고 \"삭제된 상태\"(`postStatus=REMOVED`)로 변합니다.\n\n")
     @DeleteMapping("/post/{postId}")
     public void removePost(@PathVariable Long postId) {
         postService.removePost(postId);
