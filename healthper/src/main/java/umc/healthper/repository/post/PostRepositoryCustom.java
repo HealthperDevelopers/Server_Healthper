e3558aaa6eb0bb8ad5c4fc9c28572de9a7050c50
Wo-ogie <siwall0105@gmail.com>
2022-08-22 22:23:09 +0900

    docs: Post 메서드 명세 작성

--- healthper/src/main/java/umc/healthper/repository/post/PostRepositoryCustom.java
+++ healthper/src/main/java/umc/healthper/repository/post/PostRepositoryCustom.java
@@ -3,5 +3,10 @@ package umc.healthper.repository.post;
 import umc.healthper.domain.post.Post;
 
 public interface PostRepositoryCustom {
+    
+    /**
+     * 전달받은 post 객체를 삭제된 상태(postStatus=REMOVED)로 변경
+     * @param post
+     */
     void removePost(Post post);
 }
