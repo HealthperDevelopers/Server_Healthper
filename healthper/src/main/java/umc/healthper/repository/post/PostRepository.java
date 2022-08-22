d46585d9e08261efeccc68e9ef7048e676f82b68
Wo-ogie <siwall0105@gmail.com>
2022-08-22 22:23:09 +0900

    docs: Post 메서드 명 작성

--- healthper/src/main/java/umc/healthper/repository/post/PostRepository.java
+++ healthper/src/main/java/umc/healthper/repository/post/PostRepository.java
@@ -11,10 +11,24 @@ import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 
+    /**
+     * Post 단건 조회
+     * - Member Entity Fetch Join
+     * @param id must not be {@literal null}.
+     * @return 조회된 Optional Post 객체
+     */
     @Override
     @EntityGraph(attributePaths = {"member"})
     Optional<Post> findById(Long id);
 
+    /**
+     * Post 목록 조회
+     * - Member Entity Fetch Join
+     * - Paging, Sorting 기능 제공
+     * @param pageable
+     * @param postStatus
+     * @return 조회된 Post 객체들이 담긴 Page 객체
+     */
     @EntityGraph(attributePaths = {"member"})
     Page<Post> findAllByStatusNot(Pageable pageable, PostStatus postStatus);
 }
