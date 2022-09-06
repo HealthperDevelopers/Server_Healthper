40d16efc41663717bce66ca009cba4d2d96f8045
Wo-ogie <siwall0105@gmail.com>
2022-09-06 17:06:45 +0900

    Feat: 댓글 생성, 대댓글 생성 API 분리

--- healthper/src/main/java/umc/healthper/api/CommentController.java
+++ healthper/src/main/java/umc/healthper/api/CommentController.java
@@ -12,11 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import umc.healthper.domain.comment.Comment;
 import umc.healthper.domain.member.Member;
 import umc.healthper.domain.post.Post;
-import umc.healthper.dto.comment.CommentResponseDto;
-import umc.healthper.dto.comment.CreateCommentResponseDto;
-import umc.healthper.dto.comment.CreateCommentRequestDto;
-import umc.healthper.dto.comment.UpdateCommentRequestDto;
-import umc.healthper.exception.ExceptionResponse;
+import umc.healthper.dto.comment.*;
 import umc.healthper.global.argumentresolver.Login;
 import umc.healthper.service.CommentService;
 import umc.healthper.service.MemberService;
@@ -36,28 +32,27 @@ public class CommentController {
 
     @Operation(summary = "댓글 생성",
             description = "댓글 정보를 받아 새로운 댓글을 생성합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Success", content = @Content(schema = @Schema(implementation = CreateCommentResponseDto.class))),
-            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
-            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = Void.class)))
-    })
     @PostMapping("/comment")
-    public CreateCommentResponseDto saveComment(@RequestBody @Valid CreateCommentRequestDto request,
-                                                @Parameter(hidden = true) @Login Long loginMemberId) {
+    public void saveComment(@RequestBody @Valid CreateCommentRequestDto request,
+                            @Parameter(hidden = true) @Login Long loginMemberId) {
+        Member member = memberService.findById(loginMemberId);
+        Post post = postService.findPost(request.getPostId(), false);
+        Comment comment = Comment.createComment(member, post, request.getContent());
+        commentService.saveComment(comment);
+    }
+
+    @Operation(summary = "대댓글 생성",
+            description = "대댓글 정보를 받아 새로운 대댓글을 생성합니다.")
+    @PostMapping("/comment-nested")
+    public void saveComment(@RequestBody @Valid CreateNestedCommentRequestDto requestDto,
+                            @Parameter(hidden = true) @Login Long loginMemberId) {
         Member findMember = memberService.findById(loginMemberId);
-        Post findPost = postService.findPost(request.getPostId(), false);
-        Comment comment = Comment.createComment(findMember, findPost, request.getContent());
+        Post findPost = postService.findPost(requestDto.getPostId(), false);
 
-        Long parentId = request.getParentId();
-        if (parentId != null) {
-            Comment parentComment = commentService.findById(parentId);
-            parentComment.addChildComment(comment);
-        }
-
-        Long id = commentService.saveComment(comment).getId();
-        return new CreateCommentResponseDto(id);
+        Comment comment = Comment.createComment(findMember, findPost, requestDto.getContent());
+        Comment parentComment = commentService.findById(requestDto.getParentId());
+        parentComment.addChildComment(comment);
+        commentService.saveComment(comment);
     }
 
     @Operation(summary = "댓글 조회",
@@ -65,11 +60,7 @@ public class CommentController {
                     "**Response**\n\n" +
                     "- `status`: `NORMAL`, `REMOVED`(삭제된 댓글), `BLOCKED`(차단된 댓글)")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Success", content = @Content(schema = @Schema(implementation = CommentResponseDto.class))),
-            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
-            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = ExceptionResponse.class)))
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = CommentResponseDto.class)))
     })
     @GetMapping("/comment/{commentId}")
     public CommentResponseDto getComment(@PathVariable Long commentId) {
@@ -80,13 +71,6 @@ public class CommentController {
     @Operation(summary = "댓글 수정",
             description = "댓글 수정에 관한 데이터들을 전달받아 `commentId`에 해당하는 댓글을 수정합니다.\n\n" +
                     "<del>작성자만 수정이 가능합니다. (미구현)</del>")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Success"),
-            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
-            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = ExceptionResponse.class)))
-    })
     @PatchMapping("/comment/{commentId}")
     public void updateComment(@PathVariable Long commentId,
                               @RequestBody @Valid UpdateCommentRequestDto request,
@@ -98,13 +82,6 @@ public class CommentController {
             description = "`commentId`에 해당하는 댓글을 삭제합니다.\n\n" +
                     "실제로 DB에서 삭제되지는 않고 \"삭제된 상태\"(`postStatus=REMOVED`)로 변합니다.\n\n" +
                     "<del>작성자만 삭제가 가능합니다. (미구현)</del>")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Success"),
-            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
-            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(schema = @Schema(implementation = ExceptionResponse.class))),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = ExceptionResponse.class)))
-    })
     @DeleteMapping("/comment/{commentId}")
     public void removeComment(@PathVariable Long commentId,
                               @Parameter(hidden = true) @Login Long loginMemberId) {
