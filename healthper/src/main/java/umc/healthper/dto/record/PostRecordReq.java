2133134beb5ebbab40e2146c94b56df26a724c95
peterysh <52229539+peterysh@users.noreply.github.com>
2022-09-07 16:42:18 +0900

    fetch: embedded json 검증 로직. 통계 controller, service, repository 구현. Section, StatisticController, StatisticService, RecordController test code 구현(TDD)

--- healthper/src/main/java/umc/healthper/dto/record/PostRecordReq.java
+++ healthper/src/main/java/umc/healthper/dto/record/PostRecordReq.java
@@ -1,17 +1,20 @@
 package umc.healthper.dto.record;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import umc.healthper.Section;
 import umc.healthper.global.BaseExerciseEntity;
 
+import javax.annotation.PostConstruct;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class PostRecordReq {
     @NotNull
     private String comment;
@@ -19,4 +22,5 @@ public class PostRecordReq {
     private List<Section> sections;
     @NotNull
     private BaseExerciseEntity exerciseInfo;
+
 }
---
+++ healthper/src/test/java/umc/healthper/api/StatisticControllerTest.java
@@ -0,0 +1,120 @@
+package umc.healthper.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import umc.healthper.dto.statistic.DateVolumeEntity;
+import umc.healthper.dto.statistic.GetStatisticRes;
+import umc.healthper.global.login.SessionConst;
+import umc.healthper.service.statistic.StatisticService;
+import umc.healthper.service.statistic.StatisticServiceImpl;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Slf4j
+@MockBean(JpaMetamodelMappingContext.class)
+@WebMvcTest(StatisticController.class)
+class StatisticControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StatisticService service;
+
+    private static MockHttpSession session;
+    private static Long loginId;
+
+    @BeforeAll
+    static void login(){
+        loginId = 1l;
+        session = new MockHttpSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER,loginId);
+    }
+
+    @After
+    void logout(){
+        session.clearAttributes();
+    }
+
+
+    @Test
+    @DisplayName("통계기록 Controller")
+    void getStatistic() throws Exception {
+        //given
+        String exerciseName = "밴치프레스";
+        GetStatisticRes response = RegularGet();
+        given(service.getStatisticByExerciseName(loginId, exerciseName)).willReturn(response);
+
+        //when
+        mockMvc.perform(get("/statistic")
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(session)
+                .param("exerciseName", exerciseName)
+                //then
+            ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.chart").isArray())
+                .andExpect(jsonPath("$.chart[0].date").value(LocalDate.of(2022,8,20).toString()))
+                .andExpect(jsonPath("$.chart[0].volume").value(100l))
+                .andExpect(jsonPath("$.totalVolume").value(400l))
+                .andExpect(jsonPath("$.totalTime").value(600l));
+
+        verify(service).getStatisticByExerciseName(loginId, exerciseName);
+    }
+
+    private GetStatisticRes RegularGet() {
+        GetStatisticRes response = new GetStatisticRes();
+        List<DateVolumeEntity> chart = new ArrayList<>();
+        chart.add(new DateVolumeEntity(LocalDate.of(2022,8,20),100l));
+        chart.add(new DateVolumeEntity(LocalDate.of(2022,8,25),300l));
+        return response.builder().chart(chart).totalTime(600l).totalVolume(400l).build();
+    }
+
+    /**
+     * 없는 운동 조회시 빈 list 반환
+     */
+    @Test
+    @DisplayName("없는 운동 조회시")
+    void getEmptyStatistic() throws Exception {
+        //given
+        String exerciseName = "밴치프레스";
+        GetStatisticRes response = RegularGetEmpty();
+        given(service.getStatisticByExerciseName(loginId, exerciseName)).willReturn(response);
+
+        //when
+        mockMvc.perform(get("/statistic")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .session(session)
+                                .param("exerciseName", exerciseName)
+                        //then
+                ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.chart").isEmpty())
+                .andExpect(jsonPath("$.totalVolume").value(0l))
+                .andExpect(jsonPath("$.totalTime").value(0l));
+
+        verify(service).getStatisticByExerciseName(loginId, exerciseName);
+    }
+
+    private GetStatisticRes RegularGetEmpty() {
+        GetStatisticRes response = new GetStatisticRes();
+        List<DateVolumeEntity> chart = new ArrayList<>();
+        return response.builder().chart(chart).totalTime(0l).totalVolume(0l).build();
+    }
+}
