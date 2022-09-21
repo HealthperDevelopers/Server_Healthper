df4c39ebbc7ac75647c841154545764664f0744c
Wo-ogie <siwall0105@gmail.com>
2022-09-21 18:56:42 +0900

    Fix: 잘못 수정한 API 로직 변경

--- healthper/src/main/java/umc/healthper/api/LoginController.java
+++ healthper/src/main/java/umc/healthper/api/LoginController.java
@@ -9,9 +9,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import umc.healthper.domain.member.Member;
 import umc.healthper.domain.member.MemberStatus;
 import umc.healthper.exception.member.MemberNotFoundException;
@@ -23,7 +24,7 @@ import javax.servlet.http.HttpSession;
 import java.time.LocalDate;
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @Tag(name = "Login", description = "로그인/로그아웃 API")
 public class LoginController {
@@ -31,7 +32,7 @@ public class LoginController {
     private final MemberService memberService;
 
     @Operation(summary = "Login",
-            description = "kakaoId를 전달받아 로그인 합니다.\n\n" +
+            description = "kakaoId(kakaoKey)를 전달받아 로그인 합니다.\n\n" +
                     "DB에 존재하지 않는(가입한 적 없는) 회원 또는 탈퇴했던 회원인 경우에는 404 Error 응답")
     @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Success", content = @Content(schema = @Schema(implementation = List.class)))})
     @GetMapping("/login")
@@ -54,6 +55,7 @@ public class LoginController {
 
     @Operation(summary = "Logout", description = "세션에서 MEMBER 정보를 제거합니다.")
     @GetMapping("/logout")
+    @ResponseBody
     public void logout(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         if (session != null) {
